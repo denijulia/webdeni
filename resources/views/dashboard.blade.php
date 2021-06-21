0ef0a2005adf4e6f6e82b74259ab5be695217bf0
denijulia <deni.julia@undiksha.ac.id>
2021-06-22 04:15:48 +0800

    webdeni

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <div class="jumbotorn text-center">
+    <img src="kopi.jpg" width="400">
+
+    <div class="text-left">
+      <h3>Deni Coffe Shop </h3>
+      <h6>coffe shop  pada mulanya hanya menyediakan tempat untuk minum kopi dan teh dengan cepat.
+       Akan tetapi, karena perkembangan dan kebutuhan pelanggan yang semakin lama semakin kompleks dan tidak ada habisnya
+        berkembanglah coffee shop seperti yang sekarang ini.
+    
+</x-app-layout>
